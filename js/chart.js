084750c61be1a390ecec104d4708978bb0416879
Miguel Peixe <miguel@cardume.art.br>
2016-11-06 15:45:10 -0200

    change cladeChart declaration

--- js/chart.js
+++ js/chart.js
@@ -1,139 +1,135 @@
-(function($) {
-
-  function cladeChart(config) {
-    var chart = {
-      chart: {
-        type: config.type,
-        backgroundColor: null,
+function cladeChart(config) {
+  var chart = {
+    chart: {
+      type: config.type,
+      backgroundColor: null,
+      style: {
+        fontFamily: 'Ubuntu, sans-serif'
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    title: false,
+    subtitle: false,
+    legend: {
+      enabled: false
+    },
+    xAxis: {
+      type: 'category',
+      labels: {
         style: {
-          fontFamily: 'Ubuntu, sans-serif'
+          color: '#fff'
+        }
+      }
+    },
+    yAxis: {
+      title: {
+        text: config.title,
+        style: {
+          color: 'rgba(255,255,255,0.5)'
         }
       },
-      credits: {
-        enabled: false
+      labels: {
+        style: {
+          color: '#fff'
+        }
       },
-      title: false,
-      subtitle: false,
-      legend: {
-        enabled: false
-      },
-      xAxis: {
-        type: 'category',
-        labels: {
+      min: 0,
+      gridLineColor: 'rgba(0,0,0,0.1)'
+    },
+    plotOptions: {
+      series: {
+        color: config.color,
+        stacking: config.stacking,
+        groupPadding: 0.2,
+        borderWidth: 0,
+        fillOpacity: .3,
+        dataLabels: {
+          enabled: true,
+          color: '#fff',
           style: {
-            color: '#fff'
+            textShadow: false
           }
         }
-      },
-      yAxis: {
+      }
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>'
+    },
+    exporting: {
+      chartOptions: {
         title: {
-          text: config.title,
-          style: {
-            color: 'rgba(255,255,255,0.5)'
-          }
+          text: config.title
         },
-        labels: {
-          style: {
-            color: '#fff'
-          }
+        subtitle: {
+          text: config.subtitle
         },
-        min: 0,
-        gridLineColor: 'rgba(0,0,0,0.1)'
-      },
-      plotOptions: {
-        series: {
-          color: config.color,
-          stacking: config.stacking,
-          groupPadding: 0.2,
-          borderWidth: 0,
-          fillOpacity: .3,
-          dataLabels: {
-            enabled: true,
-            color: '#fff',
+        yAxis: {
+          title: false,
+          labels: {
             style: {
-              textShadow: false
+              color: '#000'
             }
           }
-        }
-      },
-      tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>'
-      },
-      exporting: {
-        chartOptions: {
-          title: {
-            text: config.title
-          },
-          subtitle: {
-            text: config.subtitle
-          },
-          yAxis: {
-            title: false,
-            labels: {
-              style: {
-                color: '#000'
-              }
+        },
+        xAxis: {
+          labels: {
+            style: {
+              color: '#000'
             }
-          },
-          xAxis: {
-            labels: {
-              style: {
-                color: '#000'
-              }
-            }
-          },
-          plotOptions: {
-            series: {
-              dataLabels: {
-                color: '#000'
-              }
+          }
+        },
+        plotOptions: {
+          series: {
+            dataLabels: {
+              color: '#000'
             }
           }
         }
       }
-    };
-    if(config.plotline) {
-      // Plot line
-      chart.yAxis.minRange = config.plotline;
-      chart.yAxis.plotLines = [{
-        value: config.plotline,
-        color: '#fff',
-        zIndex: 10,
-        dashStyle: 'shortdash',
-        width: 1,
-        label: {
-          text: config.plotlineText,
-          style: {
-            textShadow: "2px 2px rgba(0,0,0,0.3)",
-            color: '#fff',
-            fontWeight: 500,
-            textTransform: 'uppercase'
-          }
-        }
-      }];
-      // exporting plot line
-      chart.exporting.chartOptions.yAxis.plotLines = [{
-        value: config.plotline,
-        color: config.color,
-        zIndex: 10,
-        dashStyle: 'shortdash',
-        width: 3,
-        label: {
-          text: config.plotlineText,
-          style: {
-            color: '#000',
-            fontWeight: 500,
-            textTransform: 'uppercase'
-          }
-        }
-      }];
     }
-    // data
-    chart.data = {
-      csv: config.data
-    };
-    return $(config.element).highcharts(chart);
   };
-
-})(jQuery);
+  if(config.plotline) {
+    // Plot line
+    chart.yAxis.minRange = config.plotline;
+    chart.yAxis.plotLines = [{
+      value: config.plotline,
+      color: '#fff',
+      zIndex: 10,
+      dashStyle: 'shortdash',
+      width: 1,
+      label: {
+        text: config.plotlineText,
+        style: {
+          textShadow: "2px 2px rgba(0,0,0,0.3)",
+          color: '#fff',
+          fontWeight: 500,
+          textTransform: 'uppercase'
+        }
+      }
+    }];
+    // exporting plot line
+    chart.exporting.chartOptions.yAxis.plotLines = [{
+      value: config.plotline,
+      color: config.color,
+      zIndex: 10,
+      dashStyle: 'shortdash',
+      width: 3,
+      label: {
+        text: config.plotlineText,
+        style: {
+          color: '#000',
+          fontWeight: 500,
+          textTransform: 'uppercase'
+        }
+      }
+    }];
+  }
+  // data
+  chart.data = {
+    csv: config.data
+  };
+  return jQuery(config.element).highcharts(chart);
+};
