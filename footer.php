e39250b71efa5cef5648649aec04daf004c65610
Miguel Peixe <miguel@cardume.art.br>
2017-02-07 15:52:46 -0200

    i18n license

--- footer.php
+++ footer.php
@@ -13,8 +13,9 @@
   <section id="colophon-copyright">
     <div class="container">
       <div class="twelve columns">
-        <p class="copyright"><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
-          Este trabalho está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</p>
+        <p class="copyright">
+          <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="<?php _e('Create Commons License', 'clade'); ?>" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+          <?php _e('This work is licensed under a', 'clade'); ?> <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><?php _e('Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License', 'clade'); ?></a>.</p>
         <p class="social">
           <a class="fa fa-facebook-official" href="https://facebook.com/campanaderechoeducacion/" target="_blank" rel="external"></a>
           <a class="fa fa-twitter" href="https://twitter.com/redclade" target="_blank" rel="external"></a>
